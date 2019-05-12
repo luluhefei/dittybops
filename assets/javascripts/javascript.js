5aac17a75000a86c7de37d04e611e425630ae281
Lulu Wang <luluhefei@gmail.com>
2019-05-12 13:14:33 -0700

    updated js

--- assets/javascripts/javascript.js
+++ assets/javascripts/javascript.js
@@ -1,7 +1,8 @@
 window.onscroll = function() {stickynav()};
 
-var header = document.getElementById("mystickynav");
-var sticky = header.offsetTop;
+var header = document.getElementById("header");
+// using .hasOwnProperty("offsetTop") along with this sanity check allows you to safely get this value
+var sticky = header && header.hasOwnProperty("offsetTop") ? header.offsetTop : 0;
 
 function stickynav() {
   if (window.pageYOffset > sticky) {
@@ -13,19 +14,21 @@ function stickynav() {
 
 // MDB Lightbox Init
 $(function () {
-$("#mdb-lightbox-ui").load("mdb-addons/mdb-lightbox-ui.html");
+	$("#mdb-lightbox-ui").load("mdb-addons/mdb-lightbox-ui.html");
 });
 
 // MDB filter
 $(function() {
-var selectedClass = "";
-$(".filter").click(function(){
-selectedClass = $(this).attr("data-rel");
-$("#merch-gallery").fadeTo(100, 0.1);
-$("#merch-gallery div").not("."+selectedClass).fadeOut().removeClass('animation');
-setTimeout(function() {
-$("."+selectedClass).fadeIn().addClass('animation');
-$("#merch-gallery").fadeTo(300, 1);
-}, 300);
+	var selectedClass = "";
+	$(".filter").click(function(){
+	selectedClass = $(this).attr("data-rel");
+
+	$("#merch-gallery").fadeTo(100, 0.1);
+	$("#merch-gallery div").not("."+selectedClass).fadeOut().removeClass('animation');
+
+	setTimeout(function() {
+		$("."+selectedClass).fadeIn().addClass('animation');
+		$("#merch-gallery").fadeTo(300, 1);
+	}, 300);
 });
 });
